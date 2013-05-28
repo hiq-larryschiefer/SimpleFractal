66e0e61eadb9a1e823f0febfaf2965d44c3af0cb
Larry Schiefer <larry.schiefer@hiqes.com>
2013-05-27 22:13:53 -0500

    Added GPL license and copyright comment headers.

--- src/com/hiqes/SimpleFractal/MandelbrotNativeGen.java
+++ src/com/hiqes/SimpleFractal/MandelbrotNativeGen.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2013, HiQES LLC
+ * ALL RIGHTS RESERVED
+ *
+ * http://www.hiqes.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 package com.hiqes.SimpleFractal;
 
 import android.content.Context;
--- src/com/hiqes/SimpleFractal/mand_gen.rs
+++ src/com/hiqes/SimpleFractal/mand_gen.rs
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2013, HiQES LLC
+ * ALL RIGHTS RESERVED
+ *
+ * http://www.hiqes.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
 #pragma version(1)
 #pragma rs java_package_name(com.hiqes.SimpleFractal.mand_gen)
 
